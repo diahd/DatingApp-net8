3b8bc30fb33fea074a0c25ec31b7e76064e74f6c
diah <diahdwiyanti93@gmail.com>
2024-08-16 17:48:23 +0700

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//add cors
+builder.Services.AddCors();
 //dikomen karena gapake swagger
 //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
@@ -27,6 +30,9 @@ var app = builder.Build();
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
